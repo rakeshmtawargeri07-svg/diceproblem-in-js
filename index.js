110d3ab660d8b4dc36ac26651aad3feda6d90886
Rakesh <rakeshmtawargeri07@gmail.com>
2026-05-15 21:26:50 +0530

    git repo dice project

---
+++ index.js
@@ -0,0 +1,4 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+document.querySelector(".img1").setAttribute("src", randomImageSource);
